2a07f9cb2c6d5586d54b0f1c3f030cfd4cabbbef
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-01-01 13:01:17 -1000

    fixed geocent forward

--- geocent/forward.glsl
+++ geocent/forward.glsl
@@ -2,17 +2,16 @@
 const float PI = 3.141592653589793;
 
 vec3 geocent_forward (geocent_t t, vec3 p) { // lon lat height
+  vec2 r = p.xy*180.0/PI;
+  float coslon = cos(r.x), coslat = cos(r.y);
+  float sinlon = sin(r.x), sinlat = sin(r.y);
   float f = 1.0-t.b/t.a;
   float e2 = (2.0-f)*f;
-  float rlon = p.x/180.0*PI;
-  float rlat = p.y/180.0*PI;
-  float clat = cos(rlat);
-  float slat = sin(rlat);
-  float N = t.a / sqrt(1.0-e2*slat*slat);
+  float N = t.a / sqrt(1.0-e2*sin(r.y)*sin(r.y));
   return vec3(
-    (N+p.z)*clat*cos(rlon),
-    (N+p.z)*clat*sin(rlon),
-    (N*(1.0-e2)+p.z)*slat
+    -(N+p.z)*coslon*coslat,
+    (N*(1.0-e2)+p.z)*sinlat,
+    (N+p.z)*sinlon*coslat
   );
 }
 #pragma glslify: export(geocent_forward)
