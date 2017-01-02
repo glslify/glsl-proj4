42d18fcf4d04ea1dcd3d4575ab0227e0087ff503
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-01-01 14:51:25 -1000

    from degrees to radians

--- geocent/forward.glsl
+++ geocent/forward.glsl
@@ -2,12 +2,12 @@
 const float PI = 3.141592653589793;
 
 vec3 geocent_forward (geocent_t t, vec3 p) { // lon lat height
-  vec2 r = p.xy*180.0/PI;
+  vec2 r = p.xy*PI/180.0;
   float coslon = cos(r.x), coslat = cos(r.y);
   float sinlon = sin(r.x), sinlat = sin(r.y);
   float f = 1.0-t.b/t.a;
   float e2 = (2.0-f)*f;
-  float N = t.a / sqrt(1.0-e2*sin(r.y)*sin(r.y));
+  float N = t.a / sqrt(1.0-e2*sinlat*sinlat);
   return vec3(
     -(N+p.z)*coslon*coslat,
     (N*(1.0-e2)+p.z)*sinlat,
