e699f7383cb00877f52be2aa073cfa23cd76a75d
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-01-01 16:28:06 -1000

    inverse geocent works

--- geocent/inverse.glsl
+++ geocent/inverse.glsl
@@ -1,20 +1,16 @@
 #pragma glslify: geocent_t = require('./t.glsl')
 const float PI = 3.141592653589793;
 vec3 geocent_inverse (geocent_t t, vec3 p) {
-  float f = 1.0 - t.b/t.a;
-  float pp = sqrt(p.x*p.x+p.y*p.y);
-  float theta = atan(p.z*a,pp*b);
-  float sintheta = sin(theta);
-  float costheta = cos(theta);
-  float num = p.z+t.eprime*t.eprime*b*sintheta*sintheta*sintheta;
-  float denom = pp-t.e*t.e*a*costheta*costheta*costheta;
-  float lat = atan(num,denom);
-  float lon = atan(p.y,p.x);
+  float sqp = sqrt(p.x*p.x+p.z*p.z);
+  float theta = atan(p.y*t.a,sqp*t.b);
+  float sintheta = sin(theta), costheta = cos(theta);
+  float lat = atan(
+    p.y+t.eprime*t.eprime*t.b*sintheta*sintheta*sintheta,
+    sqp-t.e*t.e*t.a*costheta*costheta*costheta
+  );
+  float lon = atan(p.x,p.z);
   float sinlat = sin(lat);
-  float denom = sqrt(1.0-e*e*sinlat*sinlat);
-  float alt = (pp/cos(lat)) - (t.a / denom);
-  if (p.x < 0.0 && p.y < 0.0) lon -= PI;
-  if (p.x < 0.0 && p.y > 0.0) lon += PI;
-  return vec3(lon,lat,alt);
+  float alt = (sqp/cos(lat))-t.a/sqrt(1.0-t.e*t.e*sinlat*sinlat);
+  return vec3(lon*180.0/PI,lat*180.0/PI,alt);
 }
 #pragma glslify: export(geocent_inverse)
