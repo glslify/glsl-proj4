5ec359a3812fa3e226aaeec74e438f46a9791dce
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-01-19 17:15:20 -1000

    include data with example

--- example/gl.js
+++ example/gl.js
@@ -6,7 +6,7 @@ var proj = require('../')
 var p = proj('+proj=aea +lat_1=16 +lat_2=24 +lat_0=19 +lon_0=-157 +x_0=0 +y_0=0'
   + '+ellps=GRS80 +datum=NAD83 +units=m +no_defs')
 
-var mesh = require('./earth.json')
+var mesh = require('./hawaii.json')
 var draw = regl({
   frag: `
     precision mediump float;
