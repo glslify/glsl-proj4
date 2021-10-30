50f0f2968676accc849a71cde613799daeb4514a
substack <substack@bits.coop>
2021-10-29 18:47:19 -1000

    lat should be flipped for ortho upon visual inspection

--- ortho/forward.glsl
+++ ortho/forward.glsl
@@ -3,11 +3,11 @@ const float PI = 3.141592653589793;
 
 vec3 ortho_forward (ortho_t t, vec3 p) { // lon lat height
   vec2 r = p.xy*PI/180.0;
-  vec2 q = vec2(t.lon0,t.lat0);
+  vec2 q = vec2(t.lon0,-t.lat0);
   float coslon = cos(PI+r.x-q.x), coslat = cos(r.y);
   float sinlon = sin(PI+r.x-q.x), sinlat = sin(r.y);
   float z = t.z0+t.a*t.k0*(sin(q.y)*sinlat+cos(q.y)*coslat*coslon);
-  // In the gdal version, when z is < 0.0, the result is infinity but we can't do that here.
+  // In the gdal version, when z is > 0.0, the result is infinity but we can't do that here.
   return vec3(
     t.x0+t.a*t.k0*coslat*sinlon,
     t.y0+t.a*t.k0*(cos(q.y)*sinlat-sin(q.y)*coslat*coslon),
